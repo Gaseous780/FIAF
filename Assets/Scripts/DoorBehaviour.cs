d0fbf61807f4bc71e3fb9817e1c6a3e4b757385e
Gaseous780 <emilianogbalcarce@gmail.com>
2026-04-13 13:25:03 -0300

    DoorsCloseOpen

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -2,7 +2,26 @@ using UnityEngine;
 
 public class DoorBehaviour : MonoBehaviour
 {
-    private bool isOpenDoor = false;
+    [SerializeField] private GameObject doorModelCore;
+
+    [SerializeField] private float minPositionOnY;
+    [SerializeField] private float maxPositionOnY;
+
+    [SerializeField] private float speedOfDoors = 1f;
+
+    private bool isOpenDoor = true;
+
+    private bool closeDoor;
+
+    private float progresDoorStatus;
+
+    private void Awake()
+    {
+        maxPositionOnY = doorModelCore.transform.position.y;
+
+        closeDoor = false;
+        progresDoorStatus = 0;
+    }
 
     public void InteractDoor()
     {
@@ -15,6 +34,34 @@ public class DoorBehaviour : MonoBehaviour
             isOpenDoor= true;
         }
 
-        Debug.Log(isOpenDoor);
+        closeDoor = true;
+    }
+
+    public void MovementDoor()
+    {
+        progresDoorStatus += speedOfDoors * Time.deltaTime;
+
+        if (isOpenDoor == true)
+        {
+            doorModelCore.transform.position = new Vector3(doorModelCore.transform.position.x, Mathf.Lerp(doorModelCore.transform.position.y, maxPositionOnY, progresDoorStatus), doorModelCore.transform.position.z);
+        }
+        else
+        {
+            doorModelCore.transform.position = new Vector3(doorModelCore.transform.position.x, Mathf.Lerp(doorModelCore.transform.position.y, minPositionOnY, progresDoorStatus), doorModelCore.transform.position.z);
+        }
+    }
+
+    private void Update()
+    {
+        if (closeDoor == true)
+        {
+            MovementDoor();
+
+            if (progresDoorStatus >= 1)
+            {
+                closeDoor = false;
+                progresDoorStatus = 0;
+            }
+        }
     }
 }
