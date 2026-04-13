e04851a69363bcf573639d452db27c3d06eb451e
Gaseous780 <emilianogbalcarce@gmail.com>
2026-04-13 12:24:39 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput playerInputs;
+
+    [SerializeField] private float distance;
+
+    private void Awake()
+    {
+        playerInputs = GetComponent<PlayerInput>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveMouse (InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            
+        }
+    }
+
+    public void ClickAction (InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            RaycastHit raycast;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue(), distance));
+
+            if (Physics.Raycast(ray, out raycast))
+            {
+                Debug.Log(raycast.collider.gameObject);
+            }
+        }
+    }
+}
